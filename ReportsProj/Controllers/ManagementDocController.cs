8e50bb8ee5d62f05886482dcea8c29714de559d5
Omar Dehees <omarx2020@outlook.com>
2021-04-20 15:20:30 +0200

    الموظفين

--- ReportsProj/Controllers/ManagementDocController.cs
+++ ReportsProj/Controllers/ManagementDocController.cs
@@ -24,7 +24,8 @@ namespace ReportsProj.Controllers
         //-- Test_adv_Selected  Params>> /ManagementDoc?P1=Selected&P2=18,19,20&P3=اعلان%20التوظيف&P4=admin 
         //-- Test_EmpCard_V1  Params>> /ManagementDoc?P1=P_EmpCard_V1&P2=171&P3=Employee20%Card&P4=admin 
         //-- Test_EmpCard_V2  Params>> /ManagementDoc?P1=P_EmpCard_V2&P2=171&P3=Employee20%Card&P4=admin 
-        //-- Test_EmployemntContract  Params>> /ManagementDoc?P1=P_EmployemntContract&P2=171&P3=عقد%20توظيف&P4=admin 
+        //-- Test_EmployemntContract  Params>> /ManagementDoc?P1=P_EmployemntContract&P2=171&P3=عقد%20توظيف&P4=admin  
+        //-- Test_EmployemntContract_All  Params>> /ManagementDoc?P1=P_All_EmploymentContract&P3=عقود%20التوظيف&P4=admin
         //-- Test_Interviews   Params>> /ManagementDoc?P1=P_Interviews&P2=1,2,3&P3=المقابلات&P4=admin
         //-- Test_Interviews_All   Params>> /ManagementDoc?P1=P_Interview_All&P3=المقابلات&P4=admin
         //-- Test_ApplyToJob   Params>> /ManagementDoc?P1=P_ApplyToJob&P2=310&P3=التقدم%20لوظيفة&P4=admin
@@ -137,6 +138,8 @@ namespace ReportsProj.Controllers
 
                 case "P_ApplyToJob":
                     return View("ApplyToJob");
+                case "P_All_EmploymentContract":
+                    return View("All_EmploymentContract");
 
                 case "P_VacationRequest":
                     if (string.IsNullOrEmpty(P5) || string.IsNullOrEmpty(P6))
---
+++ ReportsProj/Reports/ManagementDoc/Rep_All_EmploymentContract.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace ReportsProj.Reports.ManagementDoc
+{
+    public partial class Rep_All_EmploymentContract
+    {
+        public Rep_All_EmploymentContract()
+        {
+            InitializeComponent();
+        }
+    }
+}
